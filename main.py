d14df35897d029701f5be46af6b9a15e0c6241b2
NxtPerfect <alakaxan@protonmail.com>
2023-11-04 23:03:03 +0100

    Added Tests, working on parsing json

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import os
 import time
 import json
-import datetime
 
 '''Run redshift, and slowly fade it into the next light
 Use json for setting up times
 One json object contains time - hour:minute, hue - 1100-5000k, brightness - 0.0-1.0'''
 
 # DISPLAY=:0.0
-
-if __name__ == "__main__":
-    current_time = time.localtime()
-    os.system("redshift -P -O {color} -b {brightness}")
+# Get sections of time with their color and brightness
 
 
 def parseJson(path: str):
@@ -19,14 +15,59 @@ def parseJson(path: str):
     data = json.load(f)
     # get each section, put into array, each contains time as actual time, color and brightness
     sections = []
-    for key, obj in data:
-        sections.append(datetime.datetime.strptime(
-            obj['time'], '%H:%M').strftime('%H:%M'))
+
+    # Check how to read from json when you have more objects, for text config this is pointless
+    for key in data:
+        value = data[key]
+
+    sections.append(
+        tuple([data['time'], data['color'], data['brightness']]))
     f.close()
 
+    return sections
 
-def calculateChange(time: str, color: int, brightness: float):
+
+def calculateChange(color: int, brightness: float, currentColor: int, currentBrightness: float):
     # Add duration of color into the json
     # Increasing the temp should take 10 minutes
-    # Decreasing should take 1h
-    pass
+
+    # Time during which we want to blend from one color and brightness to another
+    blendTime = 10 * 60
+
+    colorDiffPerFrame: int = int((color - currentColor)/blendTime)
+    brightnessDiffPerFrame: float = (
+        brightness - currentBrightness)/blendTime
+
+    return [colorDiffPerFrame, brightnessDiffPerFrame]
+
+
+def changeColorBrightness(color: int, brightness: float):
+    try:
+        os.system(f'redshift -P -O {color} -b {brightness}')
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    currentTime = time.time()
+    currentColor: int = 5500
+    currentBrightness: float = 1.0
+
+    [targetTime, targetColor, targetBrightness] = parseJson("./config.json")
+
+    [colorDiff, brightnessDiff] = calculateChange(
+        targetColor, targetBrightness, currentColor, currentBrightness)
+
+    nextColor = currentColor
+    nextBrightness = currentBrightness
+
+    while True:
+        if ((targetTime+3600) != time.time):
+            time.sleep(30)
+            continue
+
+        nextColor += colorDiff
+        nextBrightness += brightnessDiff
+
+        changeColorBrightness(nextColor, nextBrightness)
+        time.sleep(20)
