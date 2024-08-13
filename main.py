618322e3d7a7308894a28031cfe9f6373fd151de
NxtPerfect <alakaxan@protonmail.com>
2024-08-13 10:51:40 +0200

    Rewriting the script from scratch
    Correctly finds time range to be in
    correctly changes temperature and brightness using redshift

--- main.py
+++ main.py
@@ -1,118 +1,84 @@
 #!/usr/bin/env python3
-from math import floor
-import os
+from os import system
 import time
 import json
-import datetime
-
-'''Run redshift, and slowly fade it into the next light
-Use json for setting up times
-One json object contains time - hour:minute, hue - 1100-5000k, brightness - 0.0-1.0
-Current issue is how can we read actual time from .json, to match either a string, or datetime structure
-Also read all the times and manage that'''
+from datetime import datetime
+from collections import defaultdict
 
 # DISPLAY=:0.0
 # Get sections of time with their color and brightness
 
 BLEND_TIME = 60
+JSON_PATH = "./config.json"
+
+def main():
+    print("Parsing json")
+    sections = parseJson(JSON_PATH)
+    print("Getting current time")
+    currentTime = getCurrentTime()
+    print("Changing temp and brightness")
+    changeTempAndBrightness(currentTime, sections)
 
 def parseJson(path: str):
-    f = open(path)
-    data = json.load(f)
+    try:
+        f = open(path)
+        data = json.load(f)
+    except Exception as e:
+        print(f'Failed to load json file {path}.')
+        return
+
     # get each section, put into array, each contains time as actual time, color and brightness
     sections = []
 
     # Check how to read from json when you have more objects, for text config this is pointless
     for key in data:
-        sections.append(
-            tuple([data[key]['time'], data[key]['color'], data[key]['brightness']]))
+        sections.append([data[key]['time'], data[key]['color'], data[key]['brightness']])
     f.close()
 
     return sections
 
+def getCurrentTime():
+    timeNow = datetime.now()
+    formatedTime = timeNow.strftime("%H:%M")
 
-def calculateChange(color: int, brightness: float, currentColor: int, currentBrightness: float):
-    # Add duration of color into the json
-    # Increasing the temp should take 10 minutes
+    return formatedTime
 
-    # Time during which we want to blend from one color and brightness to another
+def changeTempAndBrightness(currentTime, sections):
+    print(f"Current time {currentTime}")
+    beginningOfRange, endOfRange = pickCurrentTimeRange(currentTime, sections)
+    # TODO: Calculate change to do, based on settings,
+    # how long before the time, assume 5 minutes for testing
+    # Then, incrementally change temp and brightness
+    # until reached, then sleep until next target time
+    temperature = sections[endOfRange][1]
+    brightness = sections[endOfRange][2]
+    redshift(temperature, brightness)
 
-    colorDiffPerFrame: int = int((color - currentColor)/BLEND_TIME)
-    brightnessDiffPerFrame: float = round((
-        brightness - currentBrightness)/BLEND_TIME, 2)
+def pickCurrentTimeRange(currentTime, sections):
+    for section in range(len(sections)):
+        nextSection = section + 1 if (section + 1) < len(sections) else -1
+        if currentTime > sections[section][0]:
+            print("Beginning of range in the future")
+            continue
+        if currentTime > sections[nextSection][0]:
+            print("Ending of range in the past or current")
+            continue
+        print(f'Picked {sections[section][0]} and {sections[nextSection][0]}')
+        return section, nextSection
+    raise Exception("No matching time range found")
 
-    print(f'Calculated color diff: {colorDiffPerFrame} and brightness diff: {brightnessDiffPerFrame}')
+def calculateChangeInTargetTempAndBrightnessToCurrentPerBlendFrame(targetTemp, targetBrightness, currentTemp, currentBrightness):
+    tempDifferencePerFrame = round((targetTemp - currentTemp) / BLEND_TIME)
+    brightDifferencePerFrame = round((targetBrightness - currentBrightness) / BLEND_TIME, 2)
 
-    return [colorDiffPerFrame, brightnessDiffPerFrame]
+    return tempDifferencePerFrame, brightDifferencePerFrame
 
-
-def changeColorBrightness(color: int, brightness: float):
-    print(f'Current brightness before change: {brightness}')
+def redshift(temp, brightness):
+    print(f"Changing to {temp} {brightness}")
     try:
-        os.system(f'redshift -P -O {color} -b {brightness}')
+        system(f'redshift -P -O {temp} -b {brightness}')
     except Exception as e:
-        print(e)
-
+        raise e
 
 if __name__ == "__main__":
-    color: int = 5500
-    brightness: float = 1.0
-
-    section = parseJson("./config.json")
-    [_targetTime, _targetColor, _targetBrightness] = [], [], []
-    for item in section:
-        _targetTime.append(item[0])
-        _targetColor.append(item[1])
-        _targetBrightness.append(item[2])
-
-    print(f'Target time: {_targetTime}')
-
-    [targetTime, targetColor, targetBrightness] = "00:00", 5500, 1.0
-    # Pick correct time
-    for x in range(len(_targetTime)-1):
-        curTime = datetime.datetime.now().strftime("%H:%M:%S")
-        if _targetTime[x+1] < curTime:
-            continue
-        # If current time is more than time[x] but less than next time
-        # set it's value to previous time, and target next one
-        if _targetTime[x] <= curTime and curTime < _targetTime[x+1]:
-            changeColorBrightness(section[x][1], section[x][2])
-            [targetTime, targetColor, targetBrightness] = section[x+1][0], section[x+1][1], section[x+1][2]
-            print(f'Target time: {targetTime} color: {targetColor}')
-
-    # # For debug purposes
-    # color = 3500
-    # brightness = 0.55
-
-    [colorDiff, brightnessDiff] = calculateChange(
-        targetColor, targetBrightness, color, brightness)
-
-    # Currently when timezone is picked, it never gets changed
-    # so once we successfully change the time once
-    # we don't get new section
-    while True:
-        # timeToChange = (datetime.datetime.strptime(targetTime, '%H:%M') -
-        #                 datetime.timedelta(minutes=BLEND_TIME)).time()
-        # If we reach that change time, how do we know?
-        timeToChange = (datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(targetTime, '%H:%M').time()) -
-                        datetime.timedelta(minutes=15))
-        currentTime = datetime.datetime.now().strftime("%H:%M")
-        print(f'Time to change at: {timeToChange.time()} current time: {currentTime}')
-        if (timeToChange.time() != time.time()):
-            # time.sleep(60) # sleep until target time
-            curTime = datetime.datetime.now()
-            timeDiff = (timeToChange - curTime).total_seconds()
-            print(f'Not the right time. Sleeping for: {floor(timeDiff)}s {timeDiff/60:.2f}m {timeDiff/3600:.2f}h')
-            time.sleep(floor((timeToChange.timestamp() - curTime.timestamp()) * 60))
-            continue
-
-        for x in range(BLEND_TIME):
-            print(
-                    f'Correct time. Changing color {color}K to {targetColor}K and brightness {brightness:.2f} to {targetBrightness:.2f}...')
-            color += colorDiff
-            brightness += round(brightnessDiff, 2)
-            brightness = round(brightness, 2)
-
-            changeColorBrightness(color, brightness)
-            print(f'Changes left {BLEND_TIME-x}. Sleeping...')
-            time.sleep((15*60)/BLEND_TIME)
+    main()
--- tests/test.py
+++ tests/test.py
@@ -5,15 +5,23 @@ import unittest
 class Test(unittest.TestCase):
     def test_calculation(self):
         self.assertEqual(calculateChange(5000, 1.0, 4400, 0.4), [(
-            600/600), (0.6/600)], f'Should be {600/600} and {0.6/600}')
+            600/60), (0.6/60)], f'Calculation expected {600/60} and {0.6/60}')
 
     def test_calculation_big(self):
         self.assertEqual(calculateChange(12000, 1.0, 4400, 0.4), [
-                         (int((12000-4400)/600)), ((1.0-0.4)/600)], f'Should be {int((12000-4400)/600)} and {(1.0-0.4)/600}')
+                         (round((12000-4400)/60)), ((1.0-0.4)/60)], f'Calculation big expected {round((12000-4400)/60)} and {round((1.0-0.4)/60,2)}')
+
+    def test_calculation_higher(self):
+        self.assertEqual(calculateChange(5000, 1.0, 7500, 1.4), [
+            (round((5000-7500)/60)), round((1.0-1.4)/60, 2)], f'Calculation higher expected {round((5000/7500)/60)} and {round((1.0-1.4)/60,2)}')
 
     def test_parse_json(self):
         self.assertEqual(parseJson("./testconfig.json"),
-                         [tuple(['6:00', 5000, 1.0])], 'Should be [6:00, 5000, 1.0]')
+                         [tuple(['6:00', 5000, 1.0])], 'Parse json expected [6:00, 5000, 1.0]')
+
+    def test_parse_no_json(self):
+        self.assertEqual(parseJson("./fake_json_file_not_exists.json"),
+                         None, 'Expected to fail')
 
 
 if __name__ == "__main__":
