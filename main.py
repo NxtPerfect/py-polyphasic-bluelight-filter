23fa5734780cba6c53bcc05c635d8e3ebc58feb4
NxtPerfect <alakaxan@protonmail.com>
2025-06-19 20:53:16 +0200

    fix: works on nixos
    fix: correctly calculate changeAmount
    feat: changed fps to 30, previous value (120) was unnecessairly high
    feat: added type hints
    fix: removed unnecessary commented code
    feat: added "in range" rather than "x < y or x > z"

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-#!/usr/bin/env python3
+#!/usr/bin/env nix-shell
+#!nix-shell -i python3 -p python3
 from os import system
 from sys import argv
 import time
 from datetime import datetime
 
-# DISPLAY=:0.0
-# Get sections of time with their color and brightness
+BLEND_TIME = 30
 
-BLEND_TIME = 120
-SECOND = 1000
+type number = int | float
+
 
 def main():
     args = argv
     if len(args) < 6:
-        print("Input all 5 arguments, current temp and brightness, as well as target time, temp and brightness.")
+        print(
+            "Input all 5 arguments, current temp and brightness, as well as target time, temp and brightness."
+        )
         return
     currentTemp, currentBrightness = int(args[1]), float(args[2])
     targetTime, targetTemp, targetBrightness = args[3], int(args[4]), float(args[5])
 
     if not all([currentTemp, currentBrightness, targetTemp, targetBrightness]):
-        print("Input all 5 arguments, current temp and brightness, as well as target time, temp and brightness.")
+        print(
+            "Input all 5 arguments, current temp and brightness, as well as target time, temp and brightness."
+        )
         return
 
     print("Calculating how many changes needed to hit target...")
     changesAmount = calculateChangesRequired(targetTime)
     tempChangePerIter = (int(targetTemp) - int(currentTemp)) / (changesAmount)
-    brightnessChangePerIter = (float(targetBrightness) - float(currentBrightness)) / (changesAmount)
+    brightnessChangePerIter = (float(targetBrightness) - float(currentBrightness)) / (
+        changesAmount
+    )
     print("Changing temp and brightness...")
     try:
         changeTempAndBrightness(
@@ -33,7 +39,7 @@ def main():
             currentTemp,
             currentBrightness,
             tempChangePerIter,
-            brightnessChangePerIter
+            brightnessChangePerIter,
         )
     except Exception as e:
         print(e)
@@ -43,13 +49,16 @@ def main():
         # and will miss it
         redshift(targetTemp, targetBrightness)
 
+
 def calculateChangesRequired(targetTime: str):
     currentTime = getCurrentTime()
     if len(targetTime.split(":")) < 3:
-        targetTime += ":0"
+        targetTime += ":00"
     parsedTargetTime = datetime.strptime(targetTime, "%H:%M:%S")
+    timeDiff = ((parsedTargetTime - currentTime)).total_seconds()
 
-    return round(((parsedTargetTime - currentTime)).total_seconds() * BLEND_TIME)
+    return round(timeDiff * BLEND_TIME)
+
 
 def getCurrentTime():
     timeNow = datetime.now()
@@ -57,25 +66,35 @@ def getCurrentTime():
 
     return datetime.strptime(formatedTime, "%H:%M:%S")
 
-def changeTempAndBrightness(changesAmount, currentTemp, currentBrightness, tempChange, brightnessChange):
-    for _ in range(changesAmount):
+
+def changeTempAndBrightness(
+    changesAmount: int,
+    currentTemp: number,
+    currentBrightness: number,
+    tempChange: number,
+    brightnessChange: number,
+):
+    for i in range(changesAmount):
         currentTemp += tempChange
         currentBrightness += brightnessChange
-        if _ % BLEND_TIME == 0:
+        if i % BLEND_TIME == 0:
             print(f"Changing to {currentTemp} {currentBrightness}")
         redshift(currentTemp, currentBrightness)
-        # fps to ms per frame to seconds
-        time.sleep((BLEND_TIME/SECOND)/SECOND)
+        time.sleep(1 / BLEND_TIME)
 
-def redshift(temp, brightness):
-    if temp < 1000 or temp > 25000:
-        raise Exception(f'Temperature not in range 1000, 25000 got: {temp}')
-    if brightness < 0 or brightness > 1.0:
-        raise Exception(f'Brightness not in range 0 1, got: {brightness}')
+
+def redshift(temp: number, brightness: number):
+    if round(temp) not in range(1000, 25000):
+        raise Exception(f"Temperature not in range 1000, 25000 got: {temp}")
+    if round(brightness * 100) not in range(0, 100):
+        raise Exception(
+            f"Brightness not in range 0 1, got: {brightness} {round(brightness*100)}"
+        )
     try:
-        system(f'redshift -P -O {temp} -b {brightness}')
+        _ = system(f"redshift -P -O {temp} -b {brightness} > /dev/null 2>&1")
     except Exception as e:
         raise e
+
 
 if __name__ == "__main__":
     main()
